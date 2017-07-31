eb09642b943184a4dceb57898e9b7a17bfd77644
Radoslav Genchev <r.genchew@gmail.com>
2017-07-31 18:23:52 +0200

    Populated a proposal list view with data

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
 	selector: 'proposal-list',
 	templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+	proposalOne: Proposal = new Proposal(15, 'Crezard Company', 'http://crezard.com', 'Angular 2 & Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+	proposalTwo: Proposal = new Proposal(13, 'Something Company', 'http://something.com', 'Angular 2 & Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+	proposalThree: Proposal = new Proposal(17, 'Boring Company', 'http://boring.com', 'Angular 2 & Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com')
+
+	proposals: Proposal[] = [
+		this.proposalOne, 
+		this.proposalTwo, 
+		this.proposalThree
+	]
+}

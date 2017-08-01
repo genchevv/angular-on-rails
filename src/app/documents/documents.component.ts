1b16386897c950bf5e7f6d444b9eb2ab9c6af1ad
Radoslav Genchev <r.genchew@gmail.com>
2017-08-01 21:47:21 +0200

    Added Bootstrap 4 Cards and some styles to the documents component page

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -14,21 +14,21 @@ export class DocumentsComponent {
 			description: "adsdasd das das",
 			file_url: "http://google.com",
 			updated_at: "11/11/16",
-			image_url: "http://google.com"
+			image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
 		},
 		{
 			title: "My Second Doc",
 			description: "adsdasd das das",
 			file_url: "http://google.com",
 			updated_at: "11/11/16",
-			image_url: "http://google.com"
+			image_url: "https://cdn.pixabay.com/photo/2015/01/21/14/14/freelancer-606762_960_720.jpg"
 		},
 		{
 			title: "My Last Doc",
 			description: "adsdasd das das",
 			file_url: "http://google.com",
 			updated_at: "11/11/16",
-			image_url: "http://google.com"
+			image_url: "http://maxpixel.freegreatpicture.com/static/photo/1x/Workplace-Apple-Freelancer-Imac-Ipad-Computer-606761.jpg"
 		}
 	]
 }
